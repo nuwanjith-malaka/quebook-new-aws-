086fec9abd78dae85429957f6121d3848ee3c1a8
Nuwanjith Malaka <nuwanjithm@gmail.com>
2022-03-09 22:53:11 +0530

    changed allowed host

--- qbooknew/settings/development.py
+++ qbooknew/settings/development.py
@@ -6,7 +6,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-3-23-99-152.us-east-2.compute.amazonaws.com']
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
